350be0015b7d8fd8c8ce87319eb56a5fab5c0ed6
Valerio Pagliarino <valerio.pagliarino@edu.unito.it>
2021-11-07 14:06:02 +0100

    files restored

---
+++ data-source/4-11-21/merge.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from sys import argv
+import numpy as np
+import pandas as pd
+
+tot_data = []
+for i in argv[1:]:
+    data = pd.read_csv(i).to_numpy().transpose()[1]
+    for j in data:
+        tot_data.append(j)
+
+np.savetxt("output_merge.txt", np.array(tot_data))
