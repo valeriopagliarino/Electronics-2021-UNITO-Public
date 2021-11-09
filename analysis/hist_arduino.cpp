44a5568e478a5ea92b5723ce1b5d6fc1c5b4d2fa
nuzzu <nuzzu@sbarbero.com>
2021-11-09 01:50:13 +0100

    Added JK truth table, modified histograms, added histograms style

--- analysis/hist_arduino.cpp
+++ analysis/hist_arduino.cpp
@@ -19,20 +19,19 @@ using namespace std;
 
 void gen_hist(DataErrors &data){
    /* Sort Data to make hist */
-    data.sort();
-    
-    cout << "min = " << data[0] << endl;
-    cout << "max = " << data[-1] << endl;
+
+    double min = data.min(), max=data.max();
+
+    cout << "min = " << min << endl;
+    cout << "max = " << max << endl;
 
 
-    const double dispersione_max = data[-1].d - data[0].d;
-    const double ampiezza_classe = 1;  /* 3 volte dato */
+    const double dispersione_max = max - min; 
+    const double ampiezza_classe = 1;
     const int n_classi = ceil(dispersione_max / ampiezza_classe);
 
     const double dispersione_ist = n_classi * ampiezza_classe;
   
-/*     static int color = 2; */
-    /* cout << "\033[1;3" + to_string(color++) + "m";  */
     cout << "Istogramma: data " << endl 
          << "\tN Classi = " << n_classi << endl 
          << "\tApprossimato da: " << dispersione_max << " / " << ampiezza_classe << " = " << dispersione_max/ampiezza_classe << endl
@@ -42,42 +41,39 @@ void gen_hist(DataErrors &data){
     TCanvas *canvas = new TCanvas("canvas data" , "canvas data", 0,0, 1920, 1080); 
     canvas->cd(); 
 
-    TH1F *hist_data = new TH1F("data","data", n_classi, data[0].d, data[-1].d + 1); 
+    /* TH1F *hist_data = new TH1F("data","data", n_classi, min, max + 1);  */
+    TH1F *hist_data = new TH1F("","data", n_classi-1, min+1, max);
    
     for(PairDatumError pde : data)
        hist_data->Fill(pde.d, 1); 
     
-    /* definisco la funzione di fit g1 come gaussiana: 
-     * [0]=normalizzazione
-     * [1]=media
-     * [2]=sigma
-     */
-    /* TF1 *gauss = new TF1(("gauss #data_" + name).c_str() , "gaus");  */
-    /* hist_data->Fit(("gauss #theta_" + name).c_str(), "L"); */
-    /* gStyle->SetOptFit(1); */
-    /*  */
-/*     */
-   
     /* Estetica grafico */
-    hist_data->SetTitle("data" ); 
-    hist_data->GetXaxis()->SetTitle("data"); 
-    hist_data->GetYaxis()->SetTitle("Ampiezza"); 
+    hist_data->SetTitle(""); 
+    hist_data->GetXaxis()->SetTitle("codice"); 
+    hist_data->GetYaxis()->SetTitle("frequenza"); 
     
     /* Range */
-    /*     hist_data->GetXaxis()->SetRangeUser(6.22, 6.60);  */
-     hist_data->GetYaxis()->SetRangeUser(0, 80000);
-/*      */
+    hist_data->GetYaxis()->SetRangeUser(0, 1000);
+
+    DataErrors frequencies;
+    cout << "Frequenze bin" << endl;
+    for(int i=1; i<n_classi; i++){
+        frequencies +=  PairDatumError(hist_data->GetBinContent(i), 0); 
+        cout << i << ": " << hist_data->GetBinContent(i) << endl;
+    }
+
+    cout << "Media: " << frequencies.mean();
+    TLine *l1 = new TLine(1, frequencies.mean().d, 15, frequencies.mean().d);
 
     /* Disegna */
-    hist_data->Draw(); 
-
-    /* PairDatumError mean (gauss->GetParameter(1), gauss->GetParError(1), "deg"); */
-    /* return mean; */
+    gStyle->SetOptStat(10); 
+    hist_data->Draw();
+    l1->Draw();
 }
 
 int hist_arduino(){
     /* Read data */
-    DataErrors data("../data-source/4-11-21/800_Hz_clock_single.txt", 0, "");
+    DataErrors data("../data-source/4-11-21/800_Hz_clock", 0, "");
     gen_hist(data);
 
     return 0;
